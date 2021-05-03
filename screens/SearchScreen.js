cd65dcbd92e7288268e270b28946f70373d80d00
Sahasra.0909 <shiaonreach@gmail.com>
2021-05-03 19:58:50 +0530

    Bedtime Stories

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,12 +1,92 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View,FlatList } from 'react-native';
+import {SearchBar} from 'react-native-elements'
 
 export default class Searchscreen extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      lastVisibleTransaction: null,
+      allTransactions: [],
+    }
+    
+  }
+  fetchMoreTransactions = async ()=>{
+    var text = this.state.search.toUpperCase()
+    var enteredText = text.split("")
+
+    
+   
+    const query = await db.collection("SaveStory").where('story','==',text).startAfter(this.state.lastVisibleTransaction).limit(10).get()
+    query.docs.map((doc)=>{
+      this.setState({
+        allTransactions: [...this.state.allTransactions, doc.data()],
+        lastVisibleTransaction: doc
+      })
+    })
+    searchTransactions= async(text) =>{
+      var enteredText = text.split("")  
+      if (enteredText[0].toUpperCase() ==='S'){
+        const story =  await db.collection("story").where('story','==',text).get()
+        transaction.docs.map((doc)=>{
+          this.setState({
+            allTransactions:[...this.state.allTransactions,doc.data()],
+            lastVisibleTransaction: doc
+          })
+        })
+      }
+      else if(enteredText[0].toUpperCase() === 'S'){
+        const transaction = await db.collection('transactions').where('studentId','==',text).get()
+        transaction.docs.map((doc)=>{
+          this.setState({
+            allTransactions:[...this.state.allTransactions,doc.data()],
+            lastVisibleTransaction: doc
+          })
+        })
+      }
+    }
+
+    
+    componentDidMount = async ()=>{
+      const query = await db.collection("transactions").limit(10).get()
+      query.docs.map((doc)=>{
+        this.setState({
+          allTransactions: [],
+          lastVisibleTransaction: doc
+        })
+      })
+    }
+    }
     render() {
       return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Text>Search</Text>
+        <View>
+          <View>
+          <SearchBar
+        placeholder="Type Here..."
+        onChangeText={(text)=>{this.setState({search:text})}}
+        value={search}
+      />
+      <TouchableOpacity
+            style = {styles.searchButton}
+            onPress={()=>{this.searchTransactions(this.state.search)}}
+          ></TouchableOpacity>
+          </View>
+           
+      <FlatList
+          data={this.state.allTransactions}
+          renderItem={({item})=>(
+            <View style={{borderBottomWidth: 2}}>
+              <Text>{"Title " + item.bookId}</Text>
+              <Text>{"Author: " + item.studentId}</Text>
+              <Text>{"Author: " + item.transactionType}</Text>
+            </View>
+          )}
+          keyExtractor= {(item, index)=> index.toString()}
+          onEndReached ={this.fetchMoreTransactions}
+          onEndReachedThreshold={0.7}
+        /> 
         </View>
+     
       );
     }
   }
